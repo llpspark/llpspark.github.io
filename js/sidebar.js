120ae53b3aa57dd1c18c6bf3ffc56c677d4d22a3
llpspark <llpspark@outlook.com>
2016-11-19 20:53:35 +0800

    weather modul

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,7 @@
 ﻿document.write("<div class=\"sidebar\">");
 document.write("<h3>Weather</h3>");
 document.write("<div style=\"text-align:left;width:100%;\">");
-document.write("<iframe name=\"weather_inc\" src=\"http://i.tianqi.com/index.php?c=code&id=7\" width=\"200\" height=\"80\" frameborder=\"0\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\"></iframe>");
+document.write("<iframe name=\"weather_inc\" src=\"http://i.tianqi.com/index.php?c=code&id=7\" width=\"220\" height=\"90\" frameborder=\"0\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\"></iframe>");
 document.write("</div>");
 
 document.write("<h3>Contact</h3>");
